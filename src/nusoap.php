c6ef948cada6a2275e25b0644f75c2ee0ef4afa7
Nathan Tobin <nathantobin@porch.com>
2025-02-19 12:48:28 -0500

    revert commit be63630

--- src/nusoap.php
+++ src/nusoap.php
@@ -4463,11 +4463,8 @@ class nusoap_server extends nusoap_base
 
             // get/set custom response tag name
             $opData = $this->wsdl->getOperationData($this->methodname);
-	    if (!isset($opData['output']['name'])) {
-		$this->debug('No output name in WSDL for operation ' . $this->methodname);
-		$this->setError('Operation ' . $this->methodname . ' not present in WSDL');
-		return false;
-	    }
+            $outputMessage = $this->wsdl->getOperationData($this->methodname)['output']['message'];
+            $this->responseTagName = $outputMessage;
             $this->debug('responseTagName: ' . $this->responseTagName . ' methodURI: ' . $this->methodURI);
 
             $this->debug('calling parser->get_soapbody()');
